c29f9b1432ef7caf620171cef394232815dbc169
Kramclam <kennethramclam@gmail.com>
2022-06-17 18:36:32 -0400

    Updates to CardTrick in playing-cards

---
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -0,0 +1,17 @@
+package com.tlglearning.playingcards;
+
+public class CardTrick {
+    public static void main(String[] args) {
+        // TODO: Create an instance of deck and shuffle it (or not).
+        // TODO: Draw cards from the deck, according to the instructions, placing every other card in the
+        //    red deck or the black deck.
+        // TODO: (Optional: shuffle the red deck and black deck)
+        // TODO: Generate a random integer between 0 and the smaller of the two deck
+        //   sizes, and swap the number of cards between the two.
+        // TODO: count the red cards in the red deck and the black cards in the black deck and compare them.
+        //   THEY MUST BE EQUAL.
+        // TODO: Sort each deck by color, suit, and rank, and print them out. along with the counts from
+        //   the previous step.
+
+    }
+}
