6797e8a2cef9381bddf87b8db9146e3f7911b89c
Kramclam <kennethramclam@gmail.com>
2022-06-19 00:29:08 -0400

    Solution to playing-cards

--- src/main/java/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -2,25 +2,82 @@ package com.tlglearning.playingcards;
 
 import com.tlglearning.playingcards.model.Card;
 import com.tlglearning.playingcards.model.Deck;
+import com.tlglearning.playingcards.model.Suit;
+
+import java.util.*;
 
 public class CardTrick {
     public static void main(String[] args) {
+        // empty deque sequences for face down piles.
+        List<Card> redPile = new ArrayList<>();
+        List<Card> blackPile = new ArrayList<>();
+        Random rng = new Random();
+        int redCount = 0;
+        int blackCount = 0;
+
         // Create an instance of deck and shuffle it (or not).
         Deck deck = new Deck();
         deck.shuffle();
 
-        // TODO: Draw cards from the deck, according to the instructions, placing every other card in the
-        //    red deck or the black deck.
+        // Draw cards from the deck, according to the instructions, placing every other card in the
+        //    red deque or the black deque.
         while (deck.getRemaining() != 0) {
-            Card p = deck.draw();
-            System.out.println(p);
+            Card indicator = deck.draw();
+            Card nextCard = deck.draw();
+            if(indicator.getSuit() == Suit.SPADES || indicator.getSuit() == Suit.CLUBS){
+                blackPile.add(nextCard);
+            }
+            else {
+                redPile.add(nextCard);
+            }
         }
 
-        // TODO: (Optional: shuffle the red deck and black deck)
-        // TODO: Generate a random integer between 0 and the smaller of the two deck
+        // (Optional: shuffle the red deque and black deque)
+        for(int i = redPile.size() - 1; i > 0; i--){
+            int j = rng.nextInt(i);
+            Card temp = redPile.get(i);
+            redPile.set(i, redPile.get(j));
+            redPile.set(j, temp);
+        }
+
+        for(int i = blackPile.size() - 1; i > 0; i--){
+            int j = rng.nextInt(i);
+            Card temp = blackPile.get(i);
+            blackPile.set(i, blackPile.get(j));
+            blackPile.set(j, temp);
+        }
+
+        // Generate a random integer between 0 and the smaller of the two deck
         //   sizes, and swap the number of cards between the two.
-        // TODO: count the red cards in the red deck and the black cards in the black deck and compare them.
+        int upperLimit = Math.min(redPile.size(), blackPile.size()); // u
+        int swapCount = rng.nextInt(upperLimit); // n
+        List<Card> headBlack = blackPile.subList(0, swapCount);
+        List<Card> headRed = redPile.subList(0, swapCount);
+
+        if(swapCount != 0){
+            for (int i = 0; i < swapCount; i++){
+                blackPile.set(i, headRed.get(i));
+                redPile.set(i, headBlack.get(i));
+            }
+        }
+
+
+        // count the red cards in the red deque and the black cards in the black deque and compare them.
         //   THEY MUST BE EQUAL.
+        for(Card c : blackPile){
+            if(c.getSuit() == Suit.HEARTS || c.getSuit() == Suit.DIAMONDS){
+                blackCount++;
+            }
+        }
+        for(Card c : redPile){
+            if(c.getSuit() == Suit.CLUBS || c.getSuit() == Suit.SPADES){
+                redCount++;
+            }
+        }
+        if(redCount == blackCount){
+            System.out.println("Trick was done correctly");
+        }
+
         // TODO: Sort each deck by color, suit, and rank, and print them out. along with the counts from
         //   the previous step.
 
