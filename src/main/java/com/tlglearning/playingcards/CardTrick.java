9e195522d048db3dc35f48f90ef1b9e99f0929b0
Kramclam <kennethramclam@gmail.com>
2022-06-22 12:47:45 -0400

    Comparator local class tally() playing-cards 1.6

--- src/main/java/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -75,6 +75,21 @@ public class CardTrick {
             }
         }
 
+        class DisplayComparator implements Comparator<Card> {
+
+            @Override
+            public int compare(Card card1, Card card2) {
+                int comparison = card1.getSuit().getColor()
+                        .compareTo(card2.getSuit().getColor());
+                comparison = (comparison != 0) ? comparison :
+                        card1.getSuit().compareTo(card2.getSuit());
+                comparison = (comparison != 0) ? comparison :
+                        card1.getRank().compareTo(card2.getRank());
+                return comparison;
+            }
+        }
+
+
         Comparator<Card> comparator = new DisplayComparator();
         Collections.sort((LinkedList<Card>)blackPile, comparator);
         Collections.sort((LinkedList<Card>)redPile, comparator);
@@ -82,21 +97,6 @@ public class CardTrick {
         System.out.printf("Black: count=%d, cards=%s%n", blackCount, blackPile);
         System.out.printf("Red: count=%d, cards=%s%n", redCount, redPile);
     }
-
-    private static class DisplayComparator implements Comparator<Card> {
-
-        @Override
-        public int compare(Card card1, Card card2) {
-            int comparison = card1.getSuit().getColor()
-                    .compareTo(card2.getSuit().getColor());
-            comparison = (comparison != 0) ? comparison :
-                    card1.getSuit().compareTo(card2.getSuit());
-            comparison = (comparison != 0) ? comparison :
-                    card1.getRank().compareTo(card2.getRank());
-            return comparison;
-        }
-    }
-
 }
         /*
         // Kenny's Implementation
